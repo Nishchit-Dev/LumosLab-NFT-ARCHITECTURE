22be81637a245f5a6fde81b385f2293dacbe2fe9
nishchit <nishchitpatel84@gmail.com>
2022-08-29 00:43:36 +0530

    build ui from scratch { Minted, -, +, Mint, Quantity }

---
+++ mintpage/src/Component/Ether.js
@@ -0,0 +1,45 @@
+import {ethers} from "ethers";
+import abi from '../Contract/Abi/abi.json'
+import { config } from "../config";
+export let tempProvider,tempSigner,tempAddress,tempContract ;
+
+export const getAddress = async() => {
+  if(window.ethereum){
+    return window.ethereum
+    .request({ method: "eth_requestAccounts" })
+    .then((res) => {
+        tempAddress = res;
+      return res;
+    })
+    .catch((err) => {
+      return err;
+    });
+  } else {
+    return {msg:"windows.ethereum is null"}
+  }
+ 
+};
+
+export const getProvider = async()=>{
+    tempProvider = new  ethers.providers.Web3Provider(window.ethereum,"any")
+    return tempProvider
+}
+
+export const getSigner = async()=>{
+    tempSigner = await tempProvider.getSigner()
+    return tempSigner;
+}
+
+export const setContract = async(signer)=>{
+    return new ethers.Contract(config.development.contractAddress,abi,signer)
+}
+
+export const getTotalSupply = async(contract)=>{
+
+    return await contract.totalSupply().then(res=>{
+        let hexToInt = parseInt(res.toHexString(),16)
+        return hexToInt;
+    }).catch(err=>{
+        return err
+    })
+}
---
+++ mintpage/src/config.js
@@ -0,0 +1,9 @@
+export const config = {
+    development:{
+        contractAddress:"0x90FC0185bCf22Be63c36EeE85d05D1Ce080e3b9e"
+    },  
+    Production:{
+        contractAddress:"0x90FC0185bCf22Be63c36EeE85d05D1Ce080e3b9e"
+    }
+}
+
